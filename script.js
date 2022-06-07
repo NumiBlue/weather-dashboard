2926d0fdd7350aba4636e6a0fdd88ffd6c5286dc
NumiBlue <thekindlyblog@gmail.com>
2022-06-07 14:21:09 -0400

    adds today's weather

---
+++ script.js
@@ -0,0 +1,15 @@
+//OpenWeather API
+const apiKey = '07a72207ba241e4886d27dddb4eef7ce';
+
+//HTML elements
+
+//Form and Stored
+
+//function to sort
+
+//local storage
+
+//load past events
+
+//store searched in loccal storage
+
